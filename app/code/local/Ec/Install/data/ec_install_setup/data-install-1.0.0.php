9dbc8ff47b53c915d422a5cd8d5f22e79f7a18c5
Anatolii Khoma <a.khoma@sysiq.com>
2014-07-01 17:26:44 +0300

    Add ru and ua store views

--- app/code/local/Ec/Install/data/ec_install_setup/data-install-1.0.0.php
+++ app/code/local/Ec/Install/data/ec_install_setup/data-install-1.0.0.php
@@ -1,3 +1,4 @@
 <?php
-
-exit("zzzzzzzzzzzzzzzz");
+/**
+ * EEc install init
+ */
---
+++ app/code/local/Ec/Install/data/ec_install_setup/data-upgrade-1.0.0-1.0.1.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Create store views
+ */
+
+/* @var $this Hastens_Install_Model_Resource_Setup */
+
+$websiteId = 1;
+$website = Mage::getModel('core/website')->load($websiteId);
+$rootId = $website->getDefaultStore()->getRootCategoryId();
+$storeGroupDefault = Mage::getModel('core/store_group')->load(1);
+$storeGroupDefault->setName('Default')
+    ->save();
+//Create Stores
+$storesEurope = array(
+    'uk_ua'    => 'Ukraine | Українська',
+    'ru'    => 'Russian | Руccкий',
+);
+
+$website = Mage::getModel('core/website')->load($websiteId);
+$groupsEurope = $website->getGroupCollection()->addFieldToFilter('name', 'Default');
+$groupEuropeId = $groupsEurope->getFirstItem()->getId();
+foreach ($storesEurope as $key => $value) {
+    $store = Mage::getModel('core/store');
+    $store->setCode($key)
+        ->setWebsiteId($websiteId)
+        ->setGroupId($groupEuropeId)
+        ->setName($value)
+        ->setIsActive(1)
+        ->save();
+}
