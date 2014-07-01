f066b5625d9e801f7e31db0a0ffe361f47e65a0f
Anatolii Khoma <a.khoma@sysiq.com>
2014-07-01 17:36:52 +0300

    Set locales for stores

---
+++ app/code/local/Ec/Install/data/ec_install_setup/data-upgrade-1.0.1-1.0.2.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Set locales (languages) for stores
+ *
+ */
+
+/** @var $this Mage_Core_Model_Resource_Setup */
+
+$installer = $this;
+$config = new Mage_Core_Model_Config();
+$stores = Mage::getModel('core/store')->getCollection();
+foreach ($stores as $store) {
+    $code = $store->getCode();
+    switch ($code) {
+        case 'uk_ua':
+            $config->saveConfig('general/locale/code', 'uk_UA', 'stores', $store->getId());
+            break;
+        case 'ru':
+            $config->saveConfig('general/locale/code', 'ru_RU', 'stores', $store->getId());
+            break;
+    }
+}
