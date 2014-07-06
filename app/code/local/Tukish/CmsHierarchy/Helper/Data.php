101c91073f94f92336a825fe2449551f29fec57b
Anatolii Khoma <a.khoma@sysiq.com>
2014-07-06 10:54:16 +0300

    Revert "Install Tukish CMS Hierarchy extension"
    This reverts commit bbe281b29cb9e19c4365acae9bae7ea50ac762ce.

--- app/code/local/Tukish/CmsHierarchy/Helper/Data.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @category    Tukish
- * @package     Tukish_CmsHierarchy
- * @version     1.2.4
- * @copyright   Copyright (c) 2014 Tukish inc
- */
-class Tukish_CmsHierarchy_Helper_Data extends Mage_Core_Helper_Abstract
-{}
