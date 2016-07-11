a55b0b3d4cebb3c350907956323a7bd4065ab20c
Anatolii Khoma <a.khoma@astoundcommerce.com>
2016-07-11 15:23:33 +0300

    HomePageModule block

---
+++ app/code/local/Ec/HomePageProducts/Block/HomePageProducts.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Class Ec_HomePageProducts_Block_HomePageProducts
+ */
+class Ec_HomePageProducts_Block_HomePageProducts extends Mage_Core_Block_Template
+{
+    /**
+     * Products
+     *
+     * @var array
+     */
+    protected $_products;
+
+    /**
+     * Get products from home page products category
+     *
+     * @return array
+     */
+    public function getProducts()
+    {
+        if (null === $this->_products) {
+            $category = Mage::getModel ('catalog/category')
+                ->getCollection ()
+                ->addAttributeToFilter ('url_key', 'home-page-products')
+                ->getFirstItem();
+
+            $products = $category
+                ->getProductCollection()
+                ->addAttributeToSelect ('*')
+                ->getItems();
+
+            $this->_products = $products;
+        }
+
+        return $this->_products;
+    }
+}
