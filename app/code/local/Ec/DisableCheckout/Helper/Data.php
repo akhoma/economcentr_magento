aec25c79a3c5adecfb2b45ad3729abfdf7f503c6
Anatolii Khoma <a.khoma@astoundcommerce.com>
2016-07-18 16:39:42 +0300

    Hide zero prices

--- app/code/local/Ec/DisableCheckout/Helper/Data.php
+++ app/code/local/Ec/DisableCheckout/Helper/Data.php
@@ -14,6 +14,11 @@ class Ec_DisableCheckout_Helper_Data extends Mage_Core_Helper_Abstract
     const XML_PATH_TO_DISABLE_CHECKOUT = 'ec_disable_checkout/settings/disable_checkout';
 
     /**
+     * Path to disable checkout
+     */
+    const XML_PATH_TO_HIDE_ZERO_PRICE = 'ec_disable_checkout/settings/hide_zero_price';
+
+    /**
      * Is Checkout Disabled
      *
      * @param null|bool|int|Mage_Core_Model_Store $store $store
@@ -22,5 +27,16 @@ class Ec_DisableCheckout_Helper_Data extends Mage_Core_Helper_Abstract
     public function isCheckoutDisabled($store = null)
     {
         return Mage::getStoreConfig(self::XML_PATH_TO_DISABLE_CHECKOUT, $store);
+    }
+
+    /**
+     * Is Zero Price Hidden
+     *
+     * @param null|bool|int|Mage_Core_Model_Store $store $store
+     * @return bool
+     */
+    public function isZeroPriceHidden($store = null)
+    {
+        return Mage::getStoreConfig(self::XML_PATH_TO_HIDE_ZERO_PRICE, $store);
     }
 }
